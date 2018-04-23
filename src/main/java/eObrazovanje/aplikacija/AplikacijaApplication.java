af8353e1379c1660350f0ad4226e2d9eb1d51e03
Rade Tontic <rtontic@gmail.com>
2018-04-23 18:13:36 +0200

    Initial commit

---
+++ src/main/java/eObrazovanje/aplikacija/AplikacijaApplication.java
@@ -0,0 +1,12 @@
+package eObrazovanje.aplikacija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacijaApplication.class, args);
+	}
+}
