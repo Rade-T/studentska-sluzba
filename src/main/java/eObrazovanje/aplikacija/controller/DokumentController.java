b4f979d0382430b6218b61fca0ffdc395efd3a98
Rade Tontic <rtontic@gmail.com>
2018-07-09 08:20:25 +0200

    Dodat upload dokumenata na backendu

--- src/main/java/eObrazovanje/aplikacija/controller/DokumentController.java
+++ src/main/java/eObrazovanje/aplikacija/controller/DokumentController.java
@@ -1,9 +1,18 @@
 package eObrazovanje.aplikacija.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,66 +20,95 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import eObrazovanje.aplikacija.model.Dokument;
 import eObrazovanje.aplikacija.service.DokumentService;
 import eObrazovanje.aplikacija.dto.DokumentDTO;
 
 @RestController
-@RequestMapping(value="/api/dokument")
+@RequestMapping(value = "/api/dokument")
 public class DokumentController {
-	
+
+	private static String UPLOAD_FOLDER = "src/main/resources/files/";
+
 	@Autowired
 	private DokumentService dokumentService;
-	
+
 	@GetMapping
-	public @ResponseBody
-    List<DokumentDTO> readAll(){
+	public @ResponseBody List<DokumentDTO> readAll() {
 		List<Dokument> dokumenti = (List<Dokument>) dokumentService.findAll();
 		List<DokumentDTO> dokumentiDTO = new ArrayList<>();
-		for (Dokument d : dokumenti){
+		for (Dokument d : dokumenti) {
 			dokumentiDTO.add(new DokumentDTO(d));
 		}
-		return dokumentiDTO;		
+		return dokumentiDTO;
 	}
-	
+
 	@GetMapping("/{id}")
-	public @ResponseBody
-    DokumentDTO readOne(@PathVariable(value="id")Integer id){
+	public @ResponseBody DokumentDTO readOne(@PathVariable(value = "id") Integer id) {
 		Dokument d = dokumentService.findOne(id);
 		return new DokumentDTO(d);
 	}
-	
+
 	@PostMapping
-	public @ResponseBody
-    DokumentDTO create (@RequestBody DokumentDTO dto){
+	public @ResponseBody DokumentDTO create(@RequestBody DokumentDTO dto) {
 		Dokument d = new Dokument();
 		d.setNaziv(dto.getNaziv());
 		dokumentService.save(d);
-		
+
 		return new DokumentDTO(d);
 	}
-	
+
 	@PutMapping("/{id}")
-	public @ResponseBody
-    DokumentDTO update(@PathVariable(value="id") Integer id, @RequestBody DokumentDTO dto){
+	public @ResponseBody DokumentDTO update(@PathVariable(value = "id") Integer id, @RequestBody DokumentDTO dto) {
 		Dokument d = dokumentService.findOne(id);
 		d.setNaziv(dto.getNaziv());
 		dokumentService.save(d);
-		
+
 		return new DokumentDTO(d);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public @ResponseBody
-    DokumentDTO delete(@PathVariable(value="id")Integer id){
+	public @ResponseBody DokumentDTO delete(@PathVariable(value = "id") Integer id) {
 		Dokument d = dokumentService.findOne(id);
 		dokumentService.remove(d);
-		
+
 		return new DokumentDTO(d);
 	}
-	
+
+	@PostMapping("/uploadFile")
+	public ResponseEntity<HttpStatus> handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+		try {
+			// Get the file and save it somewhere
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+
+	@GetMapping("/downloadFile/{filename}")
+	public StreamingResponseBody getFile(@PathVariable("filename") String filename) throws IOException {
+
+		String fileLocation = UPLOAD_FOLDER + filename + ".pdf";
+		InputStream inputStream = new FileInputStream(new File(fileLocation));
+		return outputStream -> {
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+				outputStream.write(data, 0, nRead);
+			}
+			inputStream.close();
+		};
+		
+	}
 
 }
